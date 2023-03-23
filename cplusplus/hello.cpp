c3f673fe9bbe5f3b92dece5035006e8ea49bb06b
sujung00061 <sujung00061@naver.com>
2023-03-23 17:31:59 +0900

    feat:230323 variable lesson

---
+++ cplusplus/hello.cpp
@@ -0,0 +1,38 @@
+#include <iostream> //#<-전처리기
+int main()
+{
+    // int a=1;
+// int -정수
+// 
+//     bool bool123=true;
+//     bool123=false;
+// //처음 bool 자료형 입력시 앞에서 안써줘도 됨. bool- 값이 있으면 true, 없으면 false.
+//     char char123='a';
+//     int int123=1;
+//     float float123=4.15;
+// int a=1;
+// int b=2;
+
+// float f=3.5;
+// float g=2.5;
+// std::cout<<a+b<<"\n"<<f+g;
+
+// bool c1=true;
+// std::cout<<c1<<"\n";
+
+// bool c2=false;
+// std::cout<<c2<<"\n";
+
+
+// char d='a';
+// std::cout<<d<<"\n";
+
+// char e='b';
+// std::cout<<e<<"\n";
+
+
+
+std::string str1="안녕?";
+std::cout<< str1<<"\n";
+//    return 0;
+}
