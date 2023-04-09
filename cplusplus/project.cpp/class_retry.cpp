b006f92dba7b829785c6c3bcbe6a88d2be938d4a
sujung00061 <sujung00061@naver.com>
2023-04-09 12:00:12 +0900

    conflict

--- cplusplus/project.cpp/class_retry.cpp
+++ cplusplus/project.cpp/class_retry.cpp
@@ -89,13 +89,13 @@ int main(){
             cout<<o->Answer()<<endl;
             answer=o->Answer();
             delete o;
-
         }
         else if(a=='*'){
             Operator *o=new Multiply(x,y);
             cout<<o->Answer()<<endl;
             answer=o->Answer();
             delete o;
+            
         }
         else if(a=='/'){
             Operator *o=new Divide(x,y);
@@ -103,24 +103,25 @@ int main(){
             answer=o->Answer();
             delete o;
         }
+
+        
         cout<<"----------------------------------------------------"<<endl;
         while(1){
             cout<<"연산을 계속 진행하시겠습니까?(Y: 계속, AC: 초기화, EXIT: 종료): ";
             cin>>b;
-            if(b=="Y" || b=="AC" || b=="EXIT"){
-                if(b=="Y"){
-                    x=answer;
-                    break;
-                }
-                if(b=="AC"){
-                    cout<<"숫자를 입력해주세요 : ";
-                    cin>>x;
-                    break;
-                }
-                if(b=="EXIT"){
-                    cout<<"총 시도한 횟수: "<<Operator::getCnt();
-                    return 0;
-                }
+
+            if(b=="Y"){
+                x=answer;
+                break;
+            }
+            else if(b=="AC"){
+                cout<<"숫자를 입력해주세요 : "<<endl;
+                cin>>x;
+                break;
+            }
+            else if(b=="EXIT"){
+                cout<<"총 시도한 횟수: "<<(Operator::getCnt())/2;
+                return 0;
             }
             else{
                 cout<<"잘못된 입력입니다. 다시 입력해주세요."<<endl;
--- cplusplus/project.cpp/make_a_game.cpp
+++ cplusplus/project.cpp/make_a_game.cpp
@@ -1,78 +1,212 @@
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
+#include <vector>
 using namespace std;
-class Character{
-    static int P_hp,C_hp;
-    int level;
+class Weapon{
 public:
-    void Attack(){
-        cout<<" ";
-    }
-    int getC_hp(){
-        return C_hp;
-    }
-    int getP_hp(){
-        return P_hp;
-    }
-    void Get(){
-        cout<<"무기를 줍습니다.";
-    }
-};
-class Weapon {
     int attack_level;
-    int attack_able;
-public:
-    Weapon(){
-    }
-        
 };
 class Knife : public Weapon{
-
 public:
-
-    Knife():Weapon(){
-        
+    Knife() : Weapon()
+    {
+        attack_level = 5;
+        cout << "푹, 상대 플레이어에게 명중했습니다." << endl;
     }
-
 };
-class Gun: public Weapon{
+class Gun : public Weapon{
 public:
-    Gun():Weapon(){
-
+    Gun() : Weapon()
+    {
+        attack_level = 10;
+        cout << "탕! 상대 플레이어에게 명중했습니다." << endl;
     }
-
 };
-int Character::C_hp=30;
-int Character::P_hp=30;
-int main(){
-    srand((unsigned int)time(NULL));
+class Character{
+public:
+    bool weapon = true;
+    Weapon w;
+    vector<string> v1;
+    vector<string> v2;
+    static int cnt;
+    int P_hp = 30, C_hp = 30;
     string name;
-    int i;
-    cout<<"캐릭터 이름 입력 : ";
-    cin>>name;
-    while(1){
-        cout<<"["<<name<<" turn]";
-        cout<<"어떤 동작을 하겠습니까? (1. 무기 줍기    2. 공격):";
-        cin>>i;
+    int a, level;
 
-
-
-
-        cout<<"[computer turn]";
-        int random=rand()%2+1;
-        if(random==1){
-        //무기주워용
+    void Name(){
+        cout << "캐릭터 이름 입력 : ";
+        cin >> name;
+    }
+    int Act(){
+        string x="x";
+        v1.push_back(x);
+        v2.push_back(x);
+        while (C_hp>0 && P_hp>0){
+            plusCnt();
+            weapon = true;
+            if (cnt % 2 == 1){
+                cout << endl
+                     << "[" << name << " turn]" << endl;
+                cout << "어떤 동작을 하겠습니까? (1. 무기 줍기    2. 공격):";
+                cin >> a;
+                if (a == 1){
+                    Get();
+                }
+                else if (a == 2){
+                    Attack();
+                    if (weapon == true){
+                        result();
+                    }
+                    else{
+                        continue;
+                    }
+                    end();
+                }
+                
+            }
+            else{
+                weapon = true;
+                srand((unsigned int)time(NULL));
+                int random = rand() % 2 + 1;
+                cout << endl << "[computer turn]" << endl;
+                if (random == 1){
+                    Get2();
+                }
+                else{   
+                    Attack2();
+                    if (weapon == true){
+                        result2();
+                    }
+                    else{
+                        continue;
+                    }
+                    end2();
+                }
+                
+            }
+        }
+    }
+    void Get(){
+        while (1){
+            cout << "어떤 무기를 줍겠습니까?(1:칼, 2:총):";
+            cin >> a;
+            if (a == 1){
+                for (int i = 0; i < 3; i++){
+                    v1.push_back("knife");
+                }
+                break;
+            }
+            else if (a == 2){
+                v1.push_back("gun");
+                break;
+            }
+            else{
+                cout << "잘못된 입력입니다." << endl;
+            }
+        }
+        cout << "무기를 줍습니다."<<endl;
+    }
+    void Get2(){
+        int random = rand() % 2 + 1;
+        if (random == 1){
+            for (int i = 0; i < 3; i++){
+                v2.push_back("knife");
+            }
         }
         else{
-        //무기써용
+            v2.push_back("gun");
         }
-
-
-
-
-
-
+        cout << "무기를 줍습니다."<<endl;
     }
-
+    void Attack(){
+        if(v1.back()=="x"){
+            cout << "사용할 무기가 없습니다!" << endl;
+            weapon = false;
+        }
+        else if (v1.back() == "knife"){
+            Weapon *w = new Knife();
+            level = w->attack_level;
+            delete w;
+            v1.pop_back();
+        }
+        else if (v1.back() == "gun"){
+            Weapon *w = new Gun();
+            level = w->attack_level;
+            delete w;
+            v1.pop_back();
+        }
+        else{
+            cout << "사용할 무기가 없습니다!" << endl;
+            weapon = false;
+        }
+    }
+    void Attack2(){
+        if (v2.back() == "knife"){
+            Weapon *w = new Knife();
+            level = w->attack_level;
+            delete w;
+            v2.pop_back();
+        }
+        else if (v2.back() == "gun"){
+            Weapon *w = new Gun();
+            level = w->attack_level;
+            delete w;
+            v2.pop_back();
+        }
+        else if(v2.back() =="x"){
+            cout << "사용할 무기가 없습니다!" << endl;
+            weapon = false;
+        }
+    }
+    void plusCnt(){
+        cnt++;
+    }
+    void result(){
+        
+        cout << name << "(이)가 " << "computer를 공격했습니다." << endl;
+        C_hp -= level;
+        if(C_hp<0){
+            cout << "컴퓨터의 hp가 " << level << "만큼 감소합니다." << endl
+            << "컴퓨터의 남은 hp: 0" <<endl;
+        }
+        else{
+            cout << "컴퓨터의 hp가 " << level << "만큼 감소합니다." << endl
+            << "컴퓨터의 남은 hp: " << C_hp<<endl;
+        }
+    }
+    void result2(){
+        cout << "computer가 " << name << "을(를) 공격했습니다." << endl;
+        P_hp -= level;
+        if(P_hp<0){
+            cout << name << "의 "
+             << "hp가" << level << "만큼 감소합니다." << endl
+             << name << "의 남은 hp: 0" << endl;
+        }
+        else{cout << name << "의 "
+             << "hp가" << level << "만큼 감소합니다." << endl
+             << name << "의 남은 hp: " << P_hp << endl;
+        }
+    }
+    int end(){
+        if (C_hp == 0){
+            cout << endl
+                 << "컴퓨터가 쓰러졌습니다. " << name << "승리!";
+            return 0;
+        }
+    }
+    int end2(){
+        if (P_hp == 0){
+            cout << endl
+                 << name << "(이)가 쓰러졌습니다. computer 승리!";
+            return 0;
+        }
+    }
+};
+int Character::cnt = 0;
+int main(){
+    srand((unsigned int)time(NULL));
+    Character c = Character();
+    c.Name(); // 이름 입력
+    c.Act();
 }
